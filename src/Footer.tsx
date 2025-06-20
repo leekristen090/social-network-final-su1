ff5e57e3fe406b13ddaba76cc13d7254491a731e
kristenlee <leekristen090@gmail.com>
2025-06-19 22:24:21 -0400

    update footer git link

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <Navbar expand={"lg"} className={"bg-secondary-subtle justify-content-center mt-auto"}
                 id={"sn-footer"}>
             Team Members: Kristen Lee |
-            <a href={"https://github.com/leekristen090/social-network-final"} target={"_blank"}
+            <a href={"https://github.com/leekristen090/social-network-final-su1"} target={"_blank"}
                id={"sn-react-git-link"} className={"m-1"}>
                 Frontend GitHub Link
             </a>
